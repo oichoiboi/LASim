d58b840cb71300aca30be85efc21ce13245ef646
Ryan <ryanhchoi@hotmail.com>
2024-05-05 02:06:13 -0400

    changed Stats class to be struct, and other classes dont inherit

--- class.py
+++ class.py
@@ -1,48 +1,46 @@
 from lookupvalues import *
 
 
-class Job:
+class Stats:
+    STAT_KEYS = ['AP', 'Add', 'Atkspeed', 'AwkCDR', 'AwkMult', 'Back', 'CDR1', 'CDR2', 'CDmg', 
+             'Charge', 'ChargeAS', 'Crit', 'DefRed', 'HBCDdmg', 'HC', 'HCAS', 'HM', 'Head', 
+             'HeadBack', 'MCDmg', 'Main Stat', 'Movespeed', 'SkillCDR', 'SkillMult', 'WpnPower']
+    
+    def __init__(self):
+        for arg in STAT_KEYS:
+            setattr(self, arg, 0)
+
+    def reset_stats(self):
+        for attr in self.__dict__:
+            setattr(self, attr, 0)
+
+class Job():
     def __init__(self, name):
         self.name = name
-        self.mainstat = 0
-        self.weaponpower = 0
-        self.set = ''
-        self.gear = []
+        self.stats = Stats()
         self.skills = []
-        
-
-class Skill:
+   
+class Skill():
     def __init__(self):
-        self.stacks = 0
+        self.stats = Stats()
         self.cooldown = 0
         self.casttime = 0
-        self.rune = None
 
-        
 
-class Gear:
-    def __init__(self, type=HELMET, trans_level=0, trans_grade=0, honing=0, tier='Relic', df=STATLUT):
+class Gear():
+    def __init__(self, type=None, trans_level=0, trans_grade=0, honing=0, tier=None):
+        self.stats = Stats()
         self.type = type
         self.trans_level = trans_level
         self.trans_grade = trans_grade
         self.honing = honing
         self.tier = tier
-        self.df = df
-        self.df.set_index(['Tier', 'Level'], inplace=True)
-        self.stat = self.df.loc[(self.tier, self.honing), self.type]
 
-class Simulator:
+
+class Simulator(Stats):
     def __init__(self) -> None:
         pass
 
-class Stats:
-    def __init__(self):
-        self.mainstat
-        self.weaponpower
-        self.crit
-        self.cdmg
-        self.mcdmg
-        self.attackspeed
-        self.movementspeed 
+       
 
 
--- lookupvalues.py
+++ lookupvalues.py
@@ -1,14 +1,27 @@
 import sqlite3
 import csv
 import pandas as pd
+import json
 
 CRIT_CONSTANT = 0
 MSASPEED_CONSTANT = 0
 CDRSTAT_CONSTANT = 0
 
 STATLUT = pd.read_excel('C:\LASim\mainstat_table.xlsx')
-json_data = STATLUT.to_json()
-print(json_data)
+
+STAT_KEYS = ['AP', 'Add', 'Atkspeed', 'AwkCDR', 'AwkMult', 'Back', 'CDR1', 'CDR2', 'CDmg', 
+             'Charge', 'ChargeAS', 'Crit', 'DefRed', 'HBCDdmg', 'HC', 'HCAS', 'HM', 'Head', 
+             'HeadBack', 'MCDmg', 'Main Stat', 'Movespeed', 'SkillCDR', 'SkillMult', 'WpnPower']
+
+with open('mainstat_table.json', 'r') as json_file:
+    gear_stats = json.load(json_file)
+
+def get_gear_value(data, tier, level, piece):
+    tier_data = data.get(tier, [])
+    item = next((item for item in tier_data if item['Level'] == level and piece in item), None)
+    return item[piece] if item else 0
+
+print(get_gear_value(gear_stats, 'Akkan', 26, 'Weapon'))
 
 HELMET = 'Helmet'
 SHOULDERS = 'Shoulders'
@@ -16,7 +29,6 @@ CHEST = 'Chest'
 PANTS = 'Pants'
 GLOVES = 'Gloves'
 WEAPON = 'Weapon'
-
 
 
 
