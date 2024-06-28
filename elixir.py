0cc583b64966b971530c993d2745cbcce27fbb40
Ryan <ryanhchoi@hotmail.com>
2024-06-28 18:11:50 -0400

    Elixir update - need a way to access Elixir objects by name. Dictionary?

--- elixir.py
+++ elixir.py
@@ -1,10 +1,11 @@
 from stats import Stats
-from typing import Dict, Union, List
+from typing import Dict, Union, List, Tuple
 from enums import GearType
 from dataclasses import dataclass
 """
 Define an Elixir class, and then instantiate every relevant elixir in the game
 """
+
 
 @dataclass
 class Elixir:
@@ -14,6 +15,7 @@ class Elixir:
 
     def get_value(self, mod, lvl):
         return self.mods.get(mod, 0).get(lvl, 0)
+
 
 ELIXIR_OPTIONS = {"Common": [
     'ATTACK_POWER', 'WEAPON_POWER', 'MAIN_STAT', 'STAGGER',
@@ -28,21 +30,32 @@ ELIXIR_OPTIONS = {"Common": [
 flatAP_ELX = Elixir('ATTACK_POWER', GearType.Common, {Stats.FlatAP: {1: 122, 2: 253, 3: 383, 4: 575, 5: 767}})
 wpnPwr_ELX = Elixir('WEAPON_POWER', GearType.Common, {Stats.WpnPower: {1: 236, 2: 488, 3: 740, 4: 1110, 5: 1480}})
 mainStat_ELX = Elixir('MAIN_STAT', GearType.Common, {Stats.MainStat: {1: 720, 2: 1782, 3: 2700, 4: 4050, 5: 5400}})
-awkDmg_ELX = Elixir('AWK_SKILL_DMG', GearType.Shoulder, {Stats.AwkDmg: {1: 0.012, 2: 0.0247, 3: 0.0375, 4: 0.0562, 5: 0.075}})
-bossDmg_ELX = Elixir('BOSS_DMG', GearType.Shoulder, {Stats.MultDmg: {1: 0.012, 2: 0.0247, 3: 0.0375, 4: 0.0562, 5: 0.075}})
+awkDmg_ELX = Elixir('AWK_SKILL_DMG', GearType.Shoulder,
+                    {Stats.AwkDmg: {1: 0.012, 2: 0.0247, 3: 0.0375, 4: 0.0562, 5: 0.075}})
+bossDmg_ELX = Elixir('BOSS_DMG', GearType.Shoulder,
+                     {Stats.MultDmg: {1: 0.012, 2: 0.0247, 3: 0.0375, 4: 0.0562, 5: 0.075}})
 addDmg_ELX = Elixir('ADD_DMG', GearType.Pants, {Stats.AddDmg: {1: 0.0049, 2: 0.0102, 3: 0.0155, 4: 0.0232, 5: 0.031}})
 critDmg_ELX = Elixir('CRIT_DMG', GearType.Pants, {Stats.CritDmg: {1: 0.0112, 2: 0.0231, 3: 0.035, 4: 0.0525, 5: 0.07}})
-idenGain_ELX = Elixir('SPECIALTY_GAIN', GearType.Pants, {Stats.IdentityGain: {1: 0.0064, 2: 0.0132, 3: 0.02, 4: 0.03, 5: 0.04}})
+idenGain_ELX = Elixir('SPECIALTY_GAIN', GearType.Pants,
+                      {Stats.IdentityGain: {1: 0.0064, 2: 0.0132, 3: 0.02, 4: 0.03, 5: 0.04}})
 apBuff_ELX = Elixir('ALLY_ENHANCE', GearType.Pants, {Stats.APBuff: {1: 0.0096, 2: 0.0198, 3: 0.03, 4: 0.045, 5: 0.06}})
-helmet_DPS_ELX = Elixir('HELMET_COMMON', GearType.Helmet, {Stats.MultDmg: {1: 0.0049, 2: 0.0102, 3: 0.0155, 4: 0.0232, 5: 0.031}})
-gloves_DPS_ELX = Elixir('HELMET_COMMON', GearType.Gloves, {Stats.AtkPower: {1: 0.0023, 2: 0.0047, 3: 0.0072, 4: 0.0108, 5: 0.0144}})
-helmet_SUP_ELX = Elixir('HELMET_COMMON', GearType.Helmet, {Stats.HealShield: {1: 0.0023, 2: 0.0047, 3: 0.0072, 4: 0.00108, 5: 0.0144}})
-gloves_SUP_ELX = Elixir('HELMET_COMMON', GearType.Gloves, {Stats.APBuff: {1: 0.0064, 2: 0.0132, 3: 0.02, 4: 0.03, 5: 0.04}})
+helmet_DPS_ELX = Elixir('HELMET_COMMON', GearType.Helmet,
+                        {Stats.MultDmg: {1: 0.0049, 2: 0.0102, 3: 0.0155, 4: 0.0232, 5: 0.031}})
+gloves_DPS_ELX = Elixir('HELMET_COMMON', GearType.Gloves,
+                        {Stats.AtkPower: {1: 0.0023, 2: 0.0047, 3: 0.0072, 4: 0.0108, 5: 0.0144}})
+helmet_SUP_ELX = Elixir('HELMET_COMMON', GearType.Helmet,
+                        {Stats.HealShield: {1: 0.0023, 2: 0.0047, 3: 0.0072, 4: 0.00108, 5: 0.0144}})
+gloves_SUP_ELX = Elixir('HELMET_COMMON', GearType.Gloves,
+                        {Stats.APBuff: {1: 0.0064, 2: 0.0132, 3: 0.02, 4: 0.03, 5: 0.04}})
 CRITICAL = Elixir('CRITICAL', None, {Stats.MultCDmg: {1: 0.06, 2: 0.12}})
 MASTER = Elixir('MASTER', None, {Stats.CritRate: {1: 0.07, 2: 0.07}, Stats.AddDmg: {1: 0, 2: 0.085}})
-
-
-
+BLADED_SHIELD = Elixir('MASTER', None, {Stats.MultDmg: {1: 0.04, 2: 0.12}})
+VANGUARD = Elixir('VANGUARD', None, {Stats.SkillDmg: {1: 0.03, 2: 0.03}, Stats.AtkPower: {1: 0.03, 2: 0.03},
+                                     Stats.MultDmg: {1: 0, 2: 0.05}})
+FAITH = Elixir('FAITH', None, {Stats.APBuff: {1: 0.08, 2: 0.14}, Stats.IdentityGain: {1: 0, 2: 0.05}})
+LUMINARY = Elixir('LUMINARY', None, {Stats.APBuff: {1: 0.08, 2: 0.14}, Stats.SkillCDR: {1: 0, 2: 0.05}})
+ADVANCE = Elixir('ADVANCE', None,
+                 {Stats.WpnPower: {1: 2230, 2: 2230}, Stats.MoveSpeed: {1: 0, 2: 0.08}, Stats.APBuff: {1: 0, 2: 0.06}})
 
 """
 Common
@@ -59,11 +72,23 @@ CHEST_ELX = {
 }
 """
 
+@dataclass
+class ElixirPiece:
+    piece: GearType
+    elx1: UnionElixir
+    elx2: Elixir
+    elx1_lvl: int
+    elx2_lvl: int
 
-HELMET_COMMON = {1: 0.0049, 2: 0.0102, 3: 0.0155, 4: 0.0232, 5: 0.031}
-HELMET_SETS = {'DMG': {1: 0.0049, 2: 0.0102, 3: 0.0155, 4: 0.0232, 5: 0.031},
-          'BLADED_SHIELD': {1: 0.04, 2: 0.08},
-          'CRITICAL': ''
+
+
+
+@dataclass
+class ElixirMgr:
+    elixirs: Dict[GearType, Dict[int, Elixir]]
+
+
+
 
 
 
